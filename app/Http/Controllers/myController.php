645edabe1fa4f94688837cc225ea669cea2628d5
格野 <da987987@gmail.com>
2016-08-04 16:16:02 +0800

    定義路由以及對應的controller方法，可參考controllerPractices分支內容

---
+++ app/Http/Controllers/myController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class myController extends Controller
+{
+    //在這新增對應路由的方法
+    
+    
+    
+    /**
+     * Display a listing of the resoutce.
+     */
+    
+    
+    
+    public function index()
+    {
+        //
+        return "這是由controller的index方法回傳的訊息";
+    }
+}
+/**
